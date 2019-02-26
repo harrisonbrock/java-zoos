f4aa6e7b3e1eb83a1541aae32e0b7e914c78df46
Harrison Brock <harrison@harrisonbrock.com>
2019-02-26 16:08:32 -0500

    new project started

---
+++ src/main/java/com/harrisonbrock/zoomagment/ZoomagmentApplication.java
@@ -0,0 +1,13 @@
+package com.harrisonbrock.zoomagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomagmentApplication.class, args);
+    }
+
+}
